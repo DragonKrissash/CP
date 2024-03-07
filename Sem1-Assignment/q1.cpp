b9b7c5d869db12d4dcd4b121497a10efb07510b1
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-03-07 18:55:28 +0530

    7-3-2024

---
+++ Sem1-Assignment/q1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mod(int arr[],int n){
+    
+}
+
+int avg(int arr[],int n){
+
+}
+
+int sum(int arr[],int n){
+
+}
+
+int max(int arr[],int n){
+
+}
+
+int min(int arr[],int n){
+
+}
+
+int main(){
+
+}
